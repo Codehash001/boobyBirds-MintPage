75aeea6a31b1772802f87a4a46b9d9d07a25b710
Codehash001 <codehashblockchain@gmail.com>
2023-03-22 10:50:52 +0530

    first commit

---
+++ src/pages/Mint.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '/mint.css'
+
+function Mint() {
+  return (
+    <>
+      <div className='mainContainer'>
+        <div className='navCointainer'>
+          <h1>Home</h1>
+          <h1>Mint</h1>
+        </div>
+        <div className='videoContainer'></div>
+        <div className='MintContainer'></div>
+      </div>
+    </>
+  )
+}
+
+export default Mint
