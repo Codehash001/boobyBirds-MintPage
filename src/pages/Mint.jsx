b04ce917773903aaaac8e7b896d967a964b3aebe
Codehash001 <codehashblockchain@gmail.com>
2023-03-22 12:44:23 +0530

    css fix

--- src/pages/Mint.jsx
+++ src/pages/Mint.jsx
@@ -9,7 +9,14 @@ function Mint() {
           <h1>Home</h1>
           <h1>Mint</h1>
         </div>
-        <div className='videoContainer'></div>
+        <div className='videoContainer'>
+            <video className="VideoTagHome" id="video" autoPlay loop muted>
+              <source
+                src={"./config/images/vid/Web3_cartoon.mp4"}
+                type="video/mp4"
+              />
+            </video>
+        </div>
         <div className='MintContainer'></div>
       </div>
     </>
