8289557972e61e19b8f45a1fa35b9b9d7b1f25ad
Codehash001 <codehashblockchain@gmail.com>
2023-03-22 12:53:28 +0530

    css fix

--- src/pages/Mint.jsx
+++ src/pages/Mint.jsx
@@ -5,18 +5,20 @@ function Mint() {
   return (
     <>
       <div className='mainContainer'>
-        <div className='navCointainer font-face-main'>
+        <div className='navCointainer font-Besty'>
           <h1>Home</h1>
           <h1>Mint</h1>
         </div>
+
         <div className='videoContainer'>
-            <video id="video" autoPlay loop muted>
+            <video  className='vid' id="video" autoPlay loop muted>
               <source
                 src={"/config/images/vid/Web3_cartoon.mp4"}
                 type="video/mp4"
               />
             </video>
         </div>
+
         <div className='MintContainer'></div>
       </div>
     </>
