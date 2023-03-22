974d69055dc94501ca941515816ed61d9759c653
Codehash001 <codehashblockchain@gmail.com>
2023-03-22 21:31:24 +0530

    css fdix

--- src/pages/Mint.jsx
+++ src/pages/Mint.jsx
@@ -40,14 +40,20 @@ function Mint() {
               <h1>Mint</h1>
             </div>
 
-            <div className='videoContainer'>
-              <video className='vid' id="video" autoPlay loop muted>
-                <source
-                  src={"/config/images/vid/Web3_cartoon.mp4"}
-                  type="video/mp4"
-                />
-              </video>
+            <div className="video_wrapper">
+            <div className="mint_soundlink">
+              <button className="soundlink_button" onClick={handleMutedVid}>
+                <img src="config/images/volume.png" alt="" />
+              </button>
             </div>
+
+            <video className="VideoTag" id="video" autoPlay loop muted>
+              <source
+                src={"./config/images/vid/Web3_cartoon.mp4"}
+                type="video/mp4"
+              />
+            </video>
+          </div>
 
             <div className='MintContainer font-Besty'>
               <h1 className='mint_date'>MINT DATE: March 2023. STAY TUNED!</h1>
