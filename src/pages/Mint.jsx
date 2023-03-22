1478bb474df5334f09e5c79d6008fbff5cb7b446
Codehash001 <codehashblockchain@gmail.com>
2023-03-22 20:26:38 +0530

    css fix

--- src/pages/Mint.jsx
+++ src/pages/Mint.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './mint.css'
 import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultWallets, RainbowKitProvider ,ConnectButton } from "@rainbow-me/rainbowkit";
+import { getDefaultWallets, RainbowKitProvider, ConnectButton } from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 
@@ -9,11 +9,11 @@ import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 const { chains, provider } = configureChains(
   [chain.mainnet, chain.goerli],
   [
-  jsonRpcProvider ({
-   rpc:(chain) => ({ 
-    http :'https://eth-goerli.g.alchemy.com/v2/bYwv6lWEDB1KoLyivwgn_7YhZNSOkCRy', priority:0,
-  }),
-  }),
+    jsonRpcProvider({
+      rpc: (chain) => ({
+        http: 'https://eth-goerli.g.alchemy.com/v2/bYwv6lWEDB1KoLyivwgn_7YhZNSOkCRy', priority: 0,
+      }),
+    }),
   ],
 );
 
@@ -32,47 +32,38 @@ const wagmiClient = createClient({
 function Mint() {
   return (
     <>
-      <div className='mainContainer'>
-        <div className='navCointainer font-Besty'>
-          <h1>Home</h1>
-          <h1>Mint</h1>
-        </div>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <div className='mainContainer'>
+            <div className='navCointainer font-Besty'>
+              <h1>Home</h1>
+              <h1>Mint</h1>
+            </div>
 
-        <div className='videoContainer'>
-            <video  className='vid' id="video" autoPlay loop muted>
-              <source
-                src={"/config/images/vid/Web3_cartoon.mp4"}
-                type="video/mp4"
-              />
-            </video>
-        </div>
+            <div className='videoContainer'>
+              <video className='vid' id="video" autoPlay loop muted>
+                <source
+                  src={"/config/images/vid/Web3_cartoon.mp4"}
+                  type="video/mp4"
+                />
+              </video>
+            </div>
 
-        <div className='MintContainer'>
-
-        <h1 className='mint_date'>MINT DATE: March 2023. STAY TUNED!</h1>
-
-        <div className='mintSection'>
-          <h2>Public Sale is Live!</h2>
-          <h2>Connected : 0x2d...c2c</h2>
-          <h2> 10 / 5000 NFTs Minted</h2>
-          <h2>Max Mint Amount  : 2</h2>
-          
-          <WagmiConfig client={wagmiClient}>
-      		<RainbowKitProvider chains={chains}>
-        		<ConnectButton />
-      		</RainbowKitProvider>
-    	  </WagmiConfig>
-
-          
-        </div>
-        </div>
-
-
-          
-
-
-        <h3 className="font-Besty"></h3>
-      </div>
+            <div className='MintContainer font-Besty'>
+              <h1 className='mint_date'>MINT DATE: March 2023. STAY TUNED!</h1>
+              <div className='mintSection'>
+                <h1>Public Sale is Live!</h1>
+                <h2> 10 / 5000 NFTs Minted</h2>
+                <h2>Max Mint Amount  : 2</h2>
+                <div className='buttonContainer'>
+                  <ConnectButton />
+                </div>               
+              </div>
+            </div>
+            <h3 className="font-Besty"></h3>
+          </div>
+        </RainbowKitProvider>
+      </WagmiConfig>
     </>
   )
 }
