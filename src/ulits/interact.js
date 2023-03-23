68be5e9c2b6f50ad082c7a1bc8c9f34d6558816e
Codehash001 <codehashblockchain@gmail.com>
2023-03-23 23:33:22 +0530

    bug fixing

--- src/ulits/interact.js
+++ src/ulits/interact.js
@@ -340,7 +340,7 @@ export const PublicMint = async (mintAmount) => {
     gas: String(25000 * mintAmount),
     data: nftContract.methods
       .PublicMint(mintingAmount, proof)
-      .encodeABI(),
+      .call(),
     nonce: nonce.toString(16)
   }
 
