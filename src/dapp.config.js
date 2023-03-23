d78a64964a0e3b7a986d5197343c0c38e2631d27
Codehash001 <codehashblockchain@gmail.com>
2023-03-23 18:58:05 +0530

    content update and int with mint

---
+++ src/dapp.config.js
@@ -0,0 +1,60 @@
+const RPC_URL = process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL
+
+const config = {
+  title: 'Dapp',
+  description: 'minting Dapp',
+  contractAddress: '0xc65d111405249b32Ad69c05BA63DAed1f24D99e9',
+
+  maxSupply : 3100,
+  MAX_MINT_PUBLIC: 4,
+  MAX_MINT_OG: 1,
+  MAX_MINT_WHITELIST: 4,
+
+  PublicMintCost: 0.035,
+  WhiteListMintCost : 0.03
+}
+
+// const onboardOptions = {
+//   dappId: process.env.NEXT_PUBLIC_DAPP_ID,
+//   networkId: 5, // Goerli
+//   darkMode: true,
+//   walletSelect: {
+//     description:'Plaese select a wallet',
+//     wallets: [
+//       { walletName: 'metamask', preferred: true },
+//       { walletName: 'coinbase', preferred: true },
+//       {
+//         walletName: 'walletLink',
+//         preferred: true,
+//         rpcUrl: RPC_URL,
+//         appName: 'Dapp'
+//       },
+      
+//       { walletName: 'trust', preferred: true, rpcUrl: RPC_URL },
+//       { walletName: 'gnosis'},
+//       { walletName: 'authereum' },
+
+//       {
+//         walletName: 'ledger',
+//         rpcUrl: RPC_URL
+//       },
+//       {
+//         walletName: 'lattice',
+//         rpcUrl: RPC_URL,
+//         appName: 'Dapp'
+//       },
+//       {
+//         walletName: 'keepkey',
+//         rpcUrl: RPC_URL
+//       }
+//     ]
+//   },
+//   walletCheck: [
+//     { checkName: 'derivationPath' },
+//     { checkName: 'accounts' },
+//     { checkName: 'connect' },
+//     { checkName: 'network' }
+//   ]
+// }
+
+export { config }
--- src/pages/Mint.jsx
+++ src/pages/Mint.jsx
@@ -4,6 +4,20 @@ import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, RainbowKitProvider, ConnectButton } from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+import {
+  OGMint,
+  WhitelistedMint,
+  PublicMint,
+  getTotalMinted,
+  isPaused,
+  isOGMintLive,
+  isWhitelistMintLive,
+  isPublicMintLive,
+  isValidOGAddress,
+  isValidWlAddress,
+  getNumberMinted
+} from '../ulits/interact';
+import {config} from '../dapp.config'
 
 
 const { chains, provider } = configureChains(
@@ -33,10 +47,92 @@ const wagmiClient = createClient({
 
 function Mint() {
 
-  const [mutedVid, setMutedVid] = useState(true);
+const [mutedVid, setMutedVid] = useState(true);
 const handleMutedVid = () => {
   setMutedVid(!mutedVid);
 };
+
+const [isPausedState , setIsPauseState] = useState (false);
+const [isOGstate , setIsOGState] = useState (false) ;
+const [isWLState, setIsWLState] = useState (false) ;
+const [isPublicState, setIsPublicStat] = useState (false);
+const [isValidOGUser, setIsValidOGUser] = useState(false);
+const [isValidWLUser, setIsValidWlUser] = useState(false);
+
+const [numberMinted, setNumberMinted ] = useState (0);
+const [totalMinted , setTotalMinted] = useState (0);
+
+const [status, setStatus] = useState(null)
+const [mintAmount, setMintAmount] = useState(1)
+const [isMinting, setIsMinting] = useState(false)
+
+
+useEffect(() => {
+  const init = async () => {
+    setTotalMinted(await getTotalMinted())
+
+    setIsPauseState(await isPaused())
+    setIsPublicStat(await isPublicMintLive())
+    setIsOGState(await isOGMintLive())
+    setIsWLState(await isWhitelistMintLive())
+    
+    
+  }
+
+  init()
+}, []);
+
+useEffect(() => {
+  const init = async () => {
+    setIsValidOGUser(await isValidOGAddress())
+    setIsValidWlUser(await isValidWLUser())
+    setNumberMinted(await getNumberMinted())
+    //Add max Mint
+  }
+
+  init()
+}, );
+
+const publicMintHandler = async () => {
+  setIsMinting(true)
+
+  const { success, status } = await PublicMint(mintAmount)
+
+  setStatus({
+    success,
+    message: status
+  })
+  console.log(status)
+  setIsMinting(false)
+}
+
+const OGMintHandler = async () => {
+  setIsMinting(true)
+
+  const { success, status } = await OGMint(mintAmount)
+
+  setStatus({
+    success,
+    message: status
+  })
+
+  setIsMinting(false)
+}
+
+const WlMintHandler = async () => {
+  setIsMinting(true)
+
+  const { success, status } = await WhitelistedMint(mintAmount)
+
+  setStatus({
+    success,
+    message: status
+  })
+
+  setIsMinting(false)
+}
+
+
   return (
     <>
       <WagmiConfig client={wagmiClient}>
@@ -57,21 +153,22 @@ const handleMutedVid = () => {
 
             <video className="VideoTag" id="video" autoPlay loop muted={mutedVid}>
               <source
-                src={"./config/images/vid/Web3_cartoon.mp4"}
+                src={"./config/images/vid/web3-cartoon-subtitled.mp4"}
                 type="video/mp4"
               />
             </video>
             </div>
           </div>
 
-            <div className='MintContainer font-Besty'>
-              <h1 className='mint_date'>MINT DATE: March 2023. STAY TUNED!</h1>
+            <div className='MintContainer'>
+              <h1 className='mint_date'>MINT DATE: 29th March 2023. STAY TUNED!</h1>
               <div className='mintSection'>
                 <h1>Public Sale is Live!</h1>
                 <h2> 10 / 5000 NFTs Minted</h2>
                 <h2>Max Mint Amount  : 2</h2>
                 <div className='buttonContainer'>
                   <ConnectButton />
+                  <button onClick={publicMintHandler}>Mint</button>
                 </div>               
               </div>
             </div>
