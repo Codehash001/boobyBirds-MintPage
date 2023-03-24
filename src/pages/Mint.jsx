787bc8c0702f7f2c80bc617ae3ef32fa281dafd9
Codehash001 <codehashblockchain@gmail.com>
2023-03-25 01:31:57 +0530

    bug fixing

--- src/pages/Mint.jsx
+++ src/pages/Mint.jsx
@@ -182,9 +182,7 @@ const incrementMintAmount = () => {
                     isOGstate && isValidOGUser && numberMinted < config.MAX_MINT_OG ? OGMintHandler : isWLState && isValidWLUser && numberMinted < config.MAX_MINT_WHITELIST ? WlMintHandler : publicMintHandler
                     }>{isMinting ? 'Busy...' : 'Mint'}</button> : <></>}
                 </div>
-                {status && status.success ?
-                (<h4 className='statusSuc'>{status.message}</h4>) :
-                (<h4 className='statusFail'>{status.message}</h4>) }                              
+                <h1>{status.message} </h1>                             
               </div>
               <img className="gif" src='config/images/BoobyB.gif'/>
               </div>
