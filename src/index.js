b21bf4258826ba2f6830d754f1f14b7086a090f6
Codehash001 <codehashblockchain@gmail.com>
2023-03-27 14:59:55 +0530

    bug fixing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Mint from './pages/Mint';
+import {config}  from './dapp.config'
 import {
   createBrowserRouter,
   RouterProvider,
